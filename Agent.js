1aaae4af21ceebc415db397b40128e8094fc2cde
brianhouse <brian.house@gmail.com>
2018-10-23 15:33:13 -0400

    cleaned

--- Agent.js
+++ Agent.js
@@ -14,28 +14,9 @@ class Agent {
         this.attitude += this.attitude_difference * 0.001
         if (this.attitude > 1) this.attitude = 1
         if (this.attitude < 0) this.attitude = 0
-        // if (Math.floor(this.attitude) != 0 && this.attitude != 1) {
-        if (this.attitude == NaN) {
-            console.log(this.attitude)
-        }            
     }    
 
     updateStatus() {
-
-        // // schelling segregation model
-        // let owns = 0
-        // for (let neighbor of this.neighbors) {
-        //     if (neighbor.group == this.group) {
-        //         owns += 1
-        //     }
-        // }
-        // this.homogeneity = (owns / this.neighbors.length)
-        // if (this.homogeneity < this.attitude) {
-        //     this.happy = false
-        // } else {
-        //     this.happy = true
-        // }
-
         let attitude_sum = 0
         if (this.neighbors.length == 0) {
             this.attitude_difference = 1
@@ -50,7 +31,6 @@ class Agent {
         } else {
             this.happy = false
         }
-
     }   
 
     updatePosition() {
@@ -60,20 +40,6 @@ class Agent {
         }
         let original_cell = this.cell
         let open_cells = grid.openCells()
-
-        // // schelling segregation model
-        // let best_homogeneity = this.homogeneity
-        // let best_unhappy_cell = this.cell
-        // let c = 0        
-        // while (!this.happy && c < open_cells.length) {
-        //     this.move(open_cells[c++])
-        //     this.updateStatus()
-        //     if (this.homogeneity > best_homogeneity) {
-        //         best_homogeneity = this.homogeneity
-        //         best_unhappy_cell = this.cell
-        //     }
-        // }
-
         let least_attitude_difference = this.attitude_difference
         let best_unhappy_cell = this.cell
         let c = 0
@@ -85,7 +51,6 @@ class Agent {
                 best_unhappy_cell = this.cell
             }
         }        
-
         if (!this.happy) {
             this.move(best_unhappy_cell)
         }
